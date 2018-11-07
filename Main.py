7f7fcbe1946416af76cacaa8a77e6f71560ddc1e
guy.pelc@gmail.com <guy.pelc@gmail.com>
2018-11-07 11:30:25 +0200

    added DataProccessor, as described in trackpy tutorial

--- Main.py
+++ Main.py
@@ -1,5 +1,15 @@
 from data_loader import *
+import pandas
+
+
+
+def test(path):
+    f = pandas.read_csv(path)
+    print(f.head())
+
 
 dirpath = "D:\GDrive\Lab II\WEEK 2"
-save_data_from_dir(dirpath)
+path = "D:\GDrive\Lab II\Raw Data/1.csv"
 
+# save_data_from_dir(dirpath)
+test(path)
