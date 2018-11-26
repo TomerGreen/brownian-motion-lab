d60991ad93e63835e2a03925bcb4788ceaa97924
TomerGreenberg <tomerg@cs.huji.ac.il>
2018-11-26 13:07:40 +0200

    improved r_sq by time_gap analysis and added functions for residual analysis

--- data_analyzer.py
+++ data_analyzer.py
@@ -2,7 +2,10 @@ import pandas as pd
 import trackpy as tp
 import numpy as np
 import matplotlib.pyplot as plt
+import Main
 from sklearn.linear_model import LinearRegression
+import scipy.optimize as opt
+import functools
 
 
 MAX_TIME_GAP = 50
@@ -29,34 +32,6 @@ def cancel_avg_velocity_drift(data):
     data['y'] = data['y'] - (data['avg_y_vel'] * data['frame_count'])
     return data
 
-    # Older attempt
-    """
-    # Adds two empty columns for x and y average velocities.
-    data['avg_x_vel'] = np.nan
-    data['avg_y_vel'] = np.nan
-    for particle in data.particle.unique():
-        part_data = data[data.particle == particle]
-        first_frame = part_data['frame'].min()
-        last_frame = part_data['frame'].max()
-        first_frame_row = part_data[part_data['frame'] == first_frame]
-        first_x_coor = first_frame_row.at(0, 'x')
-        first_y_coor = first_frame_row.at(0, 'y')
-        last_frame_row = part_data[part_data['frame'] == last_frame]
-        last_x_coor = last_frame_row.at(0, 'x')
-        last_y_coor = last_frame_row.at(0, 'y')
-        # The average x and y velocities in distance per frame.
-        x_drift_per_frame = (last_x_coor-first_x_coor)/(last_frame-first_frame)
-        y_drift_per_frame = (last_y_coor-first_y_coor)/(last_frame-first_frame)
-        data.loc[data.particle == particle, 'avg_x_vel'] = x_drift_per_frame
-        data.loc[data.particle == particle, 'avg_y_vel'] = y_drift_per_frame
-        data.loc[data.particle == particle, 'frames_from_start'] = data.loc[data.particle == particle, 'frame'] - first_frame
-    data['x'] = data['x'] - (data['frames_from_start'] * data['avg_x_vel'])
-    data['y'] = data['y'] - (data['frames_from_start'] * data['avg_y_vel'])
-    data = data.sort(['particle', 'frame'], ascending=[True, True])
-    print(data.head(500))
-    return data
-    """
-
 
 def get_linked_data_without_drift(datafile):
     data = pd.read_csv(datafile)
@@ -68,12 +43,38 @@ def get_linked_data_without_drift(datafile):
 
 
 def get_distance_sq_data(datafile):
-    get_linked_data_without_drift(datafile)
+    """
+    Takes a tracking data file and returns summarized data per particle.
+    :param datafile: a raw tracking data file, without linking or drift-cancelling.
+    :return: a data frame with columns: particle, r_sq, frame_gap and residual.
+    """
+    data = get_linked_data_without_drift(datafile)
+    r_sq_data = pd.DataFrame()
     for particle in data.particle.unique():
         part_data = data[data.particle == particle].loc[:, ['x', 'y']]    # x,y data for current particle.
         part_sum = get_particle_sq_distance_data(part_data)
-        plt.plot(part_sum['frame_gap'], part_sum['r_sq'])
-    plt.show()
+        part_sum['particle'] = particle
+        part_sum = add_residuals_to_particle_summary(part_sum)
+        r_sq_data = r_sq_data.append(part_sum)
+    return r_sq_data
+
+
+def get_mean_residual_by_time_frame(data, cut_quantile):
+    """
+    Takes sumarrized data per particle and returns the residual averaged by particle, per time frame.
+    This will enable us to examine systematic shift from a linear fit, by time.
+    :param data: summarized data per particle with r_sq and residual per time frame
+    :param cut_quantile: a quantile like 0.05 or 0.1. Residuals below that quantile or above 1-cut_quantile
+    will be ignored.
+    :return: mean of residuals per time frame.
+    """
+    top_quant_col_name = 'quantile_' + str(1-cut_quantile)
+    bottom_quant_col_name = 'quantile_' + str(cut_quantile)
+    data[bottom_quant_col_name] = data.groupby('frame_gap')['residual'].transform(lambda x: x.quantile(cut_quantile))
+    data[top_quant_col_name] = data.groupby('frame_gap')['residual'].transform(lambda x: x.quantile(1-cut_quantile))
+    data = data[(data['residual'] <= data[top_quant_col_name]) & (data['residual'] >= data[bottom_quant_col_name])]
+    res_by_time = data.groupby('frame_gap').agg('mean')[['residual']]
+    return res_by_time
 
 
 def get_particle_sq_distance_data(part_data):
@@ -89,12 +90,48 @@ def get_particle_sq_distance_data(part_data):
         distance_sq_series = diff_data.apply(_get_sq_distance, axis='columns')
         mean_distance_sq = distance_sq_series.mean()
         result = result.append({'frame_gap':gap, 'r_sq':mean_distance_sq}, ignore_index=True)
+        result = result.dropna()
     return result
 
 
+def add_residuals_to_particle_summary(part_sum):
+    """
+    Gets distance squared vs. time frame data for a particle, and returns a data frame of residuals
+    per time frame.
+    """
+
+    # ===== FOR LINEAR FIT USE THIS ===== #
+
+    #lin_coeff = Main.get_regression(part_sum)[0]
+    #part_sum['residual'] = part_sum['r_sq'] - part_sum['frame_gap'] * lin_coeff
+
+    # ===== FOR LINEAR AND EXPONENTIAL FIT USE THIS ===== #
+    params = get_linear_plus_exp_fit(part_sum)
+    print(params)
+    part_sum['residual'] = part_sum['r_sq'] - part_sum['frame_gap'].apply(lin_plus_exp_fit_function,
+                                                                        args=(params[0], params[1], params[2]))
+
+    return part_sum
+
+
+def linear_fit_function(t, coeff):
+    return t * coeff
+
+
+def lin_plus_exp_fit_function(t, m, c, a):
+    #print("Applying parameters: " + str(t) + ", " + str(m) + ", " + str(c) + ", " + str(a))
+    #print("just exponent is: " + str(np.exp(-a*t)))
+    expression = m * t + (c / a) * (1 - np.exp(-a * t))
+    return expression
+
+
+def get_linear_plus_exp_fit(part_sum):
+    params, covariance = opt.curve_fit(lin_plus_exp_fit_function, part_sum['frame_gap'], part_sum['r_sq'], maxfev=1500)
+    return params
+
 
 if __name__ == '__main__':
-    data = get_linked_data_without_drift('data/3.csv')
-    tp.plot_traj(data)
-    #agg_data = data[['particle', 'avg_x_vel', 'avg_y_vel']].groupby('particle').agg('mean')
-    #print(agg_data)
+    r_sq_data = get_distance_sq_data('data/8.csv')
+    res_data = get_mean_residual_by_time_frame(r_sq_data, 0.1)
+    plt.scatter(res_data.index.values, res_data['residual'])
+    plt.show()
