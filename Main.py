403b367f2ae7307cca4aeac2d5c30f5e1cf17163
guy.pelc@gmail.com <guy.pelc@gmail.com>
2018-11-26 16:05:42 +0200

    Merge branch 'master' of https://github.com/TomerGreen/brownian-motion-lab
    # Conflicts:
    #	data_analyzer.py

--- Main.py
+++ Main.py
@@ -4,11 +4,46 @@ import numpy as np
 from data_analyzer import *
 import trackpy as tp
 import pims
-
+import statsmodels.formula.api as smf
+import theoretical_model
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 RAW_DATA_PATH ='D:\\GDrive\\Lab II\\Raw Data\\week 2/100%water.csv'
 TABLE2_PATH = '100%water2.table2.csv'
 TABLE3_PATH = '100%water2.table3.csv'
+
+
+def get_chi_sq(table3_path):
+    """
+
+    :param table3_path: DataFrame with columns:
+    :return: chi_squared
+    """
+
+    df = pd.read_csv(table3_path)
+
+    raise Exception('get_chi_sq not implemented yet')
+
+def append_table3(data, particle, table3_path, particle_size):
+    """
+    :param data: dataframe
+    :param particle: int
+    :param table3_path: string
+    :param particle_size: int
+    :return:
+    """
+    part_sum = get_particle_sq_distance_data(data[data.particle == particle])
+    # write data to table_3
+    c, s, std_err = get_regression_table2(part_sum)
+
+    theory_val, theory_err = theoretical_model.get_estimated_slope()
+
+    df = pd.DataFrame([[particle, c, s, particle_size, std_err, theory_err,theory_val]],
+                      columns=['particle', 'coef', 'score', 'length', 'std_err','theory_err','theory_val'])
+    # sum_file = '100%water.table3.csv'
+    with open(table3_path, 'a') as f:
+        df.to_csv(f, header=None)
 
 
 def show_annotated_first_frame(data,img_dir_path):
@@ -27,21 +62,27 @@ def get_regression_table2(part_sum):
     per particle:
     creates linear regression for r_sq vs t,
     plots graph
-    returns coeff and r^2 score
+    returns coeff and r^2 score and stderr
     :param part_sum: dataframe with r_sq, frame_gap columns
     :return: coeff: array size 1, score: int
     """
-    x = part_sum.frame_gap
-    x = x[:, None]
-    y = part_sum.r_sq
-    y = y[:, None]
-    regr = linear_model.LinearRegression(fit_intercept=False)
-    regr.fit(x, y)
-    y_fit = regr.predict(x)
-    plt.scatter(x, y)
-    plt.plot(x, y_fit)
-    plt.show()
-    return regr.coef_[0], regr.score(x, y)
+    # x = part_sum.frame_gap
+    # x = x[:, None]
+    # y = part_sum.r_sq
+    # y = y[:, None]
+    # regr = linear_model.LinearRegression(fit_intercept=False)
+    # regr.fit(x, y)
+    # y_fit = regr.predict(x)
+    # plt.scatter(x, y)
+    # plt.plot(x, y_fit)
+    # plt.show()
+    # stderr = 0
+
+    mod = smf.ols(formula='r_sq ~ frame_gap - 1', data=part_sum)
+    res = mod.fit()
+    return res.params[0], res.rsquared, res.bse[0]
+    # return regr.coef_[0], regr.score(x, y), stderr
+
 
 
 def fit_table3(table3_path):
@@ -52,9 +93,9 @@ def fit_table3(table3_path):
     """
     regr = linear_model.LinearRegression(False)
     t = pd.read_csv(table3_path)
-    x = t.length
+    x = t.length.values
     x = x[:,None]
-    y = t.coef
+    y = t.coef.values
     y = y[:,None]
     y = [1/i for i in y]
 
@@ -78,23 +119,7 @@ def append_table2(data, particle, table2_path):
         part_sum.to_csv(f, header=None)
 
 
-def append_table3(data, particle, table3_path, particle_size):
-    """
-    :param data: dataframe
-    :param particle: int
-    :param table3_path: string
-    :param particle_size: int
-    :return:
-    """
-    part_sum = get_particle_sq_distance_data(data[data.particle == particle])
-    # write data to table_3
-    c, s = get_regression_table2(part_sum)
-    c = c[0]
-    df = pd.DataFrame([[particle, c, s, particle_size]],
-                      columns=['particle', 'coef', 'score', 'size'])
-    # sum_file = '100%water.table3.csv'
-    with open(table3_path, 'a') as f:
-        df.to_csv(f, header=None)
+
 
 
 def main(data, particle, table2_path, table3_path):
--- theoretical_model.py
+++ theoretical_model.py
@@ -2,18 +2,18 @@ import scipy.constants as const
 from math import sqrt
 
 
-def get_estimated_slope(temp, temp_err, visc, visc_err, rad, rad_error):
+def get_estimated_slope(temp, temp_err, visc, visc_err, radius, radius_error):
     """
     Returns the theoretical estimate of the slope coefficient that connects <r^2> to t, with error.
     :param temp: the temperature in Kelvin
     :param temp_err: the error in temperature, in kelvin
     :param visc: the dynamic viscosity in m^2/s
     :param visc_err: the viscosity error.
-    :param rad: the radius of the particle in meters.
-    :param rad_error: the error in particle radius, in meters
+    :param radius: the radius of the particle in meters.
+    :param radius_error: the error in particle radius, in meters
     :return: a tupple in the form (slope, slope_err) such that slope_err is the propagated error. Both return
     values are in m^2/s
     """
-    slope = (2 * const.k * temp) / (3 * const.pi * visc * rad)
-    slope_err = sqrt((temp_err / temp) ^ 2 + (visc_err / visc) ^ 2 + (rad_error / rad) ^ 2) * slope
+    slope = (2 * const.k * temp) / (3 * const.pi * visc * radius)
+    slope_err = sqrt((temp_err / temp) ^ 2 + (visc_err / visc) ^ 2 + (radius_error / radius) ^ 2) * slope
     return slope, slope_err
